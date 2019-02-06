d1bdf0a6225cd5773df81d8bdb7b6a62cb2e4b4d
Joe Rup <jrupslauskas@gmail.com>
2019-02-06 16:23:58 -0600

    Added two more unit tests

---
+++ src/test/java/CylinderTest.java
@@ -0,0 +1,53 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CylinderTest {
+
+    Cylinder cylinder1 = new Cylinder(1,3);
+    Cylinder cylinder2 = new Cylinder(2,5);
+    Cylinder cylinder3 = new Cylinder(1,10);
+
+    // Surface Area Tests
+    @Test
+    public void surfaceArea1() {
+        assertEquals(25.13f, cylinder1.surfaceArea(), 0.01);
+    }
+
+    @Test
+    public void surfaceArea2() {
+        assertEquals(87.96f, cylinder2.surfaceArea(), 0.01);
+    }
+
+    @Test
+    public void surfaceArea3() {
+        assertEquals(69.11f, cylinder3.surfaceArea(), 0.01);
+    }
+
+    // Volume Tests
+    @Test
+    public void volume1() {
+        assertEquals(9.42f, cylinder1.volume(), 0.01);
+    }
+
+    @Test
+    public void volume2() {
+        assertEquals(62.83f, cylinder2.volume(), 0.01);
+    }
+
+    @Test
+    public void volume3() {
+        assertEquals(31.41f, cylinder3.volume(), 0.01);
+    }
+
+    // Getters Tests
+    @Test
+    public void getRadius() {
+        assertEquals(1, cylinder1.getRadius(), 0);
+    }
+
+    @Test
+    public void getHeight() {
+        assertEquals(3, cylinder1.getHeight(), 0);
+    }
+}
---
+++ src/test/java/SphereTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SphereTest {
+
+    @Test
+    public void surfaceArea() {
+    }
+
+    @Test
+    public void volume() {
+    }
+
+    @Test
+    public void getRadius() {
+    }
+}
