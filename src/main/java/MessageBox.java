96bc00b256672bf4eee8be7d3add4a7d00693181
Joe Rup <jrupslauskas@gmail.com>
2019-02-17 12:28:16 -0600

    Updated comment

--- src/main/java/MessageBox.java
+++ src/main/java/MessageBox.java
@@ -11,5 +11,7 @@ public class MessageBox implements Dialog {
         JOptionPane.showMessageDialog(null, message, title, 1);
 
         return 0;
+        // return JOptionPane.OK_OPTION
+        // Per converstaion with you Josh, I'm choosing to returning 0 rather than the JOptionPane.OK_Option. Either seems to work though
     }
 }
